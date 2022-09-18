81fb6a2b1021cb3d0811bc33ab299e807958110a
ggbadza <ggbadza@gmail.com>
2022-09-19 01:13:27 +0900

    설정파일

--- run.py
+++ run.py
@@ -1,6 +1,47 @@
+import configparser
 from crawling import nyaa_crawling
 from torrent import torrent_down
 
-url = 'https://nyaa.si/?f=0&c=0_0&q=%5BSubsPlease%5D+%26+1080'
+
+properties = configparser.ConfigParser()
+properties.read('config.ini')
+
+#if 'CONFIG' in properties:
+#    config=properties["CONFIG"]
+#else :
+#    config={}
+#config.setdefault('url','https://nyaa.si/?f=0&c=0_0&q=%5BSubsPlease%5D+%26+1080') #기본 url 설정
+#properties.set("CONFIG", "url", config["url"])
+
+if 'TORRENT' in properties:
+    torrent=properties["TORRENT"]
+else :
+    torrent={}
+torrent.setdefault('torrentlist',[])
+torrent.setdefault('recent','0')
+properties.set("TORRENT", "torrentlist", torrent["torrentlist"])
+properties.set("TORRENT", "recent", torrent["recent"])
+
+if 'ACCOUNT' in properties:
+    account=properties["ACCOUNT"]
+else :
+    account={}
+
+account.setdefault('id','admin')
+account.setdefault('pw','djemals')
+properties.set('ACCOUNT', "id", account["id"])
+properties.set('ACCOUNT', "pw", account["pw"])
+
+url=config['url']
+
+
+
+with open('config.ini', "w") as f:
+    properties.write(f)
+
+
 
 torrentlist=nyaa_crawling(url)
+
+print(torrentlist)
+
--- torrent.py
+++ torrent.py
@@ -1,6 +1,6 @@
 from qbittorrent import Client
 
-def torrent_down(magnet,path):
+def torrent_down(id,pw,magnet,path):
     ub=Client("http://127.0.0.1:8080/")
-    ub.login("admin","djemals")
+    ub.login(id,pw)
     ub.download_from_link(magnet,savepath=path)
